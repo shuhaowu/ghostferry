cc0551a0e464bcf11d763fac1b2f778070150de0
Shuhao Wu <shuhao@shuhaowu.com>
2018-09-11 15:39:57 -0400

    Integration test running Ghostferry binary
    Instead of running integration tests directly in Go, we run it in Ruby
    by calling Ghostferry as a subprocess. The idea is that we would
    eventually be able to test the interrupt/resume work more easily as well
    as making integration tests easier to write.
    
    In order to allow the ruby tests to inject race conditions for testing
    purposes, we need a way for the ruby code to pause and restart the
    execution of Go code at strategic locations. This is done with a local
    Unix socket. At locations of interests, the Go code will send some
    string via the Unix socket to the Ruby code. The ruby code will perform
    some sort of callback (such as injecting data, or locking a row) and
    then send a command back to the Go code via the same Unix socket,
    allowing it to continue executing. Note that each time we send some
    status to the ruby server, we do it in a new connection. This is to
    avoid race conditions if one shared connection was used between all the
    goroutines we employ inside Ghostferry.
    
    Stdout and stderr are captured and they can be asserted
    against/examined, more helper methods can be created later to give
    better structures for the stdout/stderr output such as being able to
    recognize a panic, or recognize particular stages of execution from
    within the logs.
    
    Lastly, the intention is to replace all integration tests with this
    framework in the future as it would likely be more robust and cause less
    issues such as goroutine leaks during the go test run. However, for the
    time being we will have both integration tests in Go and Ruby.

---
+++ test/integration/ruby/ghostferry_integration.rb
@@ -0,0 +1,13 @@
+require "thread"
+
+module GhostferryIntegration
+  ALPHANUMERICS = ("0".."9").to_a + ("a".."z").to_a + ("A".."Z").to_a
+  def self.rand_data(length: 32)
+    ALPHANUMERICS.sample(32).join("") + "👻⛴️"
+  end
+end
+
+require_relative "ghostferry_integration/db_manager"
+require_relative "ghostferry_integration/data_writer"
+require_relative "ghostferry_integration/ghostferry"
+require_relative "ghostferry_integration/test_case"
